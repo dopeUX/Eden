2007c0692b7f48671853cbed70084ac76cfb43aa
UXNation <uxnation.inc@gmail.com>
2022-04-20 10:20:43 +0530

    workspace

--- src/components/WorkspacePage.jsx
+++ src/components/WorkspacePage.jsx
@@ -4,5 +4,20 @@ export default function WorkspacePage(){
     return <div className="workspace-section">
        <h1>Let's set up a home for all your work</h1>
        <h3 className="workspace-base">You can always create another workspace later</h3>
+       
+       <h3 className="hero-label">Workspace Name</h3>
+       <input type="text" className="hero-input" placeholder="Eden"/>
+    
+       <h3 className="hero-label">Workspace URL <span>(optional)</span></h3>
+       <div className="workspace-url-sec">
+         <div className="workspace-url-sec-url">
+           <h3>www.eden.com/</h3>
+           <input type="text" className="hero-input" placeholder="Example"/>
+         </div> 
+         
+
+       </div>
+        
+    
     </div>
 }
